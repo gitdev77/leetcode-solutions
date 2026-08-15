0bed98771204e7c5acfc1a7150d3863175a503d9
Devansh <professionaldevanshsharma@gmail.com>
2026-08-15 15:49:46 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (96.16%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+     void preorder(TreeNode* root,int level, vector<int>& ans){
+      if (root == NULL){
+        return;
+      }
+      if(ans.size()< level){
+        ans.push_back(root->val);
+      }
+      preorder(root->right,level+1,ans);
+      preorder(root->left,level +1, ans);
+     
+     }
+
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+     preorder(root, 1,ans); //l 1 se start hai na 
+        return ans; 
+    }
+};
